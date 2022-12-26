21afc7c0f827a48cd78ac16f142476181dae6d03
teng wang <wangteng0829@gmail.com>
2022-12-26 21:37:33 +0800

    tengwang: 1.change to cluster secret

--- src/main/java/io/micrc/core/gradle/plugin/basic/ProjectConfigure.java
+++ src/main/java/io/micrc/core/gradle/plugin/basic/ProjectConfigure.java
@@ -139,7 +139,7 @@ public class ProjectConfigure {
             DependencyHandler dependencies = proj.getDependencies();
             dependencies.add("implementation", "org.springframework.boot:spring-boot-starter");
             // runtime core
-            dependencies.add("implementation", "io.micrc.core:micrc-core:v0.0.1");
+            dependencies.add("implementation", "io.micrc.core:micrc-core:v0.0.1-20221226-4");
             dependencies.add("implementation", "io.micrc.core:micrc-annotations:v0.0.1");
             // persistence annotations
             dependencies.add("implementation", "jakarta.persistence:jakarta.persistence-api:2.2.3");
