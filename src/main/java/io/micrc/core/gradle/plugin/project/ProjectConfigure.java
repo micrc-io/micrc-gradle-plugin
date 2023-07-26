ec3a89513c24d672c65a80e76a8bf0344faf25c1
hyosunghan <ihyosung@qq.com>
2023-07-26 13:43:59 +0800

    upgrade core version

--- src/main/java/io/micrc/core/gradle/plugin/project/ProjectConfigure.java
+++ src/main/java/io/micrc/core/gradle/plugin/project/ProjectConfigure.java
@@ -145,7 +145,7 @@ public class ProjectConfigure {
             DependencyHandler dependencies = proj.getDependencies();
             dependencies.add("implementation", "org.springframework.boot:spring-boot-starter");
             // runtime core
-            dependencies.add("implementation", "io.micrc.core:micrc-core:v0.0.12");
+            dependencies.add("implementation", "io.micrc.core:micrc-core:v0.0.13");
             dependencies.add("implementation", "io.micrc.core:micrc-annotations:v0.0.3");
             // persistence annotations
             dependencies.add("implementation", "jakarta.persistence:jakarta.persistence-api:2.2.3");
