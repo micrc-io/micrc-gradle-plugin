b0862bcbd262327c97b9aad079b359b8d08a8558
hyosunghan <ihyosung@qq.com>
2024-05-28 17:25:27 +0800

    upgrade

--- src/main/java/io/micrc/core/gradle/plugin/project/ProjectConfigure.java
+++ src/main/java/io/micrc/core/gradle/plugin/project/ProjectConfigure.java
@@ -151,7 +151,7 @@ public class ProjectConfigure {
             DependencyHandler dependencies = proj.getDependencies();
             dependencies.add("implementation", "org.springframework.boot:spring-boot-starter");
             // runtime core
-            dependencies.add("implementation", "io.micrc.core:micrc-core:v0.0.40");
+            dependencies.add("implementation", "io.micrc.core:micrc-core:v0.0.41");
             dependencies.add("implementation", "io.micrc.core:micrc-annotations:v0.0.9");
             // shedlock for schedule distributed lock
             dependencies.add("implementation", "net.javacrumbs.shedlock:shedlock-spring:4.42.0");
